c897ec57a33d61e1077fe84d09c627508c5b2aec
Ignatius Henriyanto Primai Renda <ignatius.henriyanto@ui.ac.id>
2023-03-07 23:12:57 +0700

    Dev

--- propensi/settings.py
+++ propensi/settings.py
@@ -25,7 +25,7 @@ PRODUCTION = os.environ.get('DATABASE_HOST') != None
 SECRET_KEY = 'django-insecure-z8u&_f#ujoyklxi+343pq*^k0tkxgvsb5g440&tu(pc87$f#dm'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -78,14 +78,9 @@ WSGI_APPLICATION = 'propensi.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'lPH4Mruih6bG5xxp',
-        'HOST': 'db.xnxflqjznuslqpsuwrrr.supabase.co',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
-
 }
 
 
