984c5b14c07cebae468b7055adf05f0ec64df32a
Irham Wahyu Arieffadhila <irham.wahyu@ui.ac.id>
2023-03-16 10:40:23 +0700

    supplier: update views.py for list_supplier func

--- supplier/views.py
+++ supplier/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .forms import AddSupplierForm
+from .models import Supplier
 
 # Create your views here.
 def create_supplier(request):
@@ -10,3 +11,8 @@ def create_supplier(request):
         return redirect("supplier:create_supplier")
     add = AddSupplierForm()     
     return render(request, 'supplier/create_supplier.html', {'form':add})
+
+def list_supplier(request):
+    suppliers = Supplier.objects.all()
+    response = {'supplier':suppliers}
+    return render(request, 'supplier/list_supplier.html', response)
