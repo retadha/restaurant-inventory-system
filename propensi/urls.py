86157759df84dd62f1696b35d40a16d883da169c
Irham Wahyu Arieffadhila <irham.wahyu@ui.ac.id>
2023-03-16 23:23:08 +0700

    supplier: adding supplier app to settings and urls

--- propensi/urls.py
+++ propensi/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include('home.urls'), name='index')
+    path("", include('home.urls'), name='index'),
+    path("supplier/", include('supplier.urls')),
 ]
