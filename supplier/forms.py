2f9e9124dd87d16be14adf1b4f12969c2fec1cec
Irham Wahyu Arieffadhila <irham.wahyu@ui.ac.id>
2023-03-18 19:58:53 +0700

    supplier: remove forms.py

--- supplier/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Supplier
-
-class AddSupplierForm(forms.ModelForm):
-    class Meta:
-        model = Supplier
-        fields = ['nama','alamat','pic','nohp']
--- supplier/views.py
+++ supplier/views.py
@@ -1,15 +1,17 @@
 from django.shortcuts import render, redirect
-from .forms import AddSupplierForm
 from .models import Supplier
 
 # Create your views here.
 def create_supplier(request):
-    context = {}
-    form = AddSupplierForm(request.POST)
-    if form.is_valid():
-        form.save()  
-    context['form']= form
-    return render(request, "add_supplier_form.html", context)
+    if(request.POST):
+        supplier = Supplier.objects.create()
+        supplier.nama = request.POST["nama"]
+        supplier.alamat = request.POST["alamat"]
+        supplier.pic = request.POST["pic"]
+        supplier.nohp = request.POST["nohp"]
+        supplier.save()
+        return redirect("/supplier/list_supplier")
+    return render(request, 'add_supplier_form.html')
 
 def list_supplier(request):
     suppliers = Supplier.objects.all()
@@ -17,7 +19,6 @@ def list_supplier(request):
     return render(request, 'list_supplier.html', response)
 
 def update_supplier(request,id):
-    context = {}
     supplier = Supplier.objects.get(id_supplier=id)
     if(request.POST):
         supplier.nama = request.POST["nama"]
@@ -25,7 +26,7 @@ def update_supplier(request,id):
         supplier.pic = request.POST["pic"]
         supplier.nohp = request.POST["nohp"]
         supplier.save()
-    context['Supplier']= supplier
+        return redirect("/supplier/list_supplier")
     return render(request, 'update_supplier_form.html', {'supplier':supplier})
 
 def get_supplier(request,id):
