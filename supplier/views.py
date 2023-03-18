0f0d380184e109a76c2cafae20eaff4b0f45d5b5
Irham Wahyu Arieffadhila <irham.wahyu@ui.ac.id>
2023-03-18 12:10:46 +0700

    supplier: preparing backend update, get, delete

--- supplier/views.py
+++ supplier/views.py
@@ -5,14 +5,29 @@ from .models import Supplier
 # Create your views here.
 def create_supplier(request):
     context = {}
-    form = AddSupplierForm(request.POST or None, request.FILES or None)
+    form = AddSupplierForm(request.POST)
     if form.is_valid():
         form.save()  
     context['form']= form
     return render(request, "add_supplier_form.html", context)
 
-
 def list_supplier(request):
     suppliers = Supplier.objects.all()
     response = {'suppliers':suppliers}
     return render(request, 'list_supplier.html', response)
+
+def update_supplier(request,id):
+    supplier = Supplier.objects.get(id=id)
+    form = AddSupplierForm(request.POST, instance=supplier)
+    if form.is_valid():  
+        form.save()
+    return render(request, 'update_supplier.html', supplier)
+
+def get_supplier(request,id):
+        supplier = Supplier.objects.get(id=id)  
+        return render(request,'get_supplier.html', {'supplier':supplier})
+
+def delete_supplier(request,id):
+    supplier = Supplier.objects.get(id=id)
+    supplier.delete()
+    return redirect("/list_supplier")
