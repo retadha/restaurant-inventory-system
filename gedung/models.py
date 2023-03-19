053e00c1ec2b5f93667567162a92e63f73880e22
retadha <retadha.rumi@gmail.com>
2023-03-19 21:13:26 +0700

    Edit field status di gedung

--- gedung/models.py
+++ gedung/models.py
@@ -4,7 +4,7 @@ class Gedung(models.Model):
     id_gedung = models.AutoField(primary_key=True, unique=True)
     nama = models.CharField(max_length=100)
     alamat = models.CharField(max_length=100)
-    status = models.CharField(max_length=5)
+    status = models.CharField(max_length=15, choices=[('0', 'GUDANG PUSAT'), ('1', 'RESTORAN')])
 
     def __str__(self):
         return self.nama
