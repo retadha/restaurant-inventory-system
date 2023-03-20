6c86ad8791a5b62331d4d3f3765c30cd07077d5b
Ignatius Henriyanto Primai Renda <ignatius.henriyanto@ui.ac.id>
2023-03-20 09:09:24 +0700

    Squashed commit of the following:
    commit 1f68abd3caf917c62f58a0152af443125bfe4e15
    Author: Ignatius Henriyanto Primai Renda <ignatius.henriyanto@ui.ac.id>
    Date:   Sun Mar 19 20:56:02 2023 +0700
    
        add datatable to employee list
    
    commit 0ace68123e810bccc31502f6ac81fb9d80473998
    Author: Ignatius Henriyanto Primai Renda <ignatius.henriyanto@ui.ac.id>
    Date:   Sun Mar 19 20:21:19 2023 +0700
    
        fix null error + login required
    
    commit 462a03d2f593abbc74c2ef26a5c9df56fc470447
    Author: Ignatius Henriyanto Primai Renda <ignatius.henriyanto@ui.ac.id>
    Date:   Sun Mar 19 13:05:15 2023 +0700
    
        Implement role based system
    
    commit a3ad2961961ea83467f455c937406e6e6c6fcd4f
    Merge: d8fdca3 a5683b0
    Author: Ignatius Henriyanto Primai Renda <ignatius.henriyanto@ui.ac.id>
    Date:   Sun Mar 19 11:58:15 2023 +0700
    
        Merge branch 'employee' into auth
    
    commit d8fdca322797c5ad881c0a144c6db635e6d59133
    Author: Ignatius Henriyanto Primai Renda <ignatius.henriyanto@ui.ac.id>
    Date:   Sat Mar 18 03:44:29 2023 +0700
    
        implement login logout

--- employee/urls.py
+++ employee/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('employees/', views.employee_list, name='employee_list'),
-    path('employee/<int:employee_id>/', views.employee_detail, name='employee_detail'),
-    path('employee/create/', views.create_employee, name='create_employee'),
+    path('list/', views.employee_list, name='employee_list'),
+    path('<int:employee_id>/', views.employee_detail, name='employee_detail'),
+    path('create/', views.create_employee, name='create_employee'),
 ]
--- propensi/settings.py
+++ propensi/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'home',
+    'login',
     'employee',
     'gedung',
 ]
@@ -67,6 +68,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'propensi.processor.role_processor',
             ],
         },
     },
