3dd2d00fd28e2144e3da39446c237d20552990e1
MAULUDI AFINA MIRZA <mauludi.afina@ui.ac.id>
2023-04-15 15:30:00 +0700

    fix fitur create inventory

--- inventory/models.py
+++ inventory/models.py
@@ -5,21 +5,10 @@ from gedung.models import Gedung
 from inventory_default.models import InventoryDefault
 
 class Inventory(models.Model):
-    id_inventory = models.AutoField(primary_key=True)
     stok = models.IntegerField(null=False, blank=False)
     threshold = models.IntegerField(null=False, blank=False)
     default_request_qty = models.IntegerField(null=False, blank=False)
 
     id_gedung = models.ForeignKey(Gedung, on_delete=models.CASCADE)
-    id_inventory_default = models.ForeignKey(InventoryDefault, on_delete=models.CASCADE)
-
-    class Meta:
-        constraints = [
-            UniqueConstraint(
-                Lower('id_gedung'),
-                Lower('id_inventory_default').desc(),
-                name='inventory_gedung_unique',
-                violation_error_message='Inventory sudah terdaftar dalam sistem'
-            ),
-        ]
+    default = models.ForeignKey(InventoryDefault, on_delete=models.CASCADE)
     
--- inventory/views.py
+++ inventory/views.py
@@ -1,49 +1,12 @@
-from django.db import IntegrityError
+
 from django.http import Http404, HttpResponseRedirect
 from django.shortcuts import redirect, render
 from django.contrib import messages
 from employee.models import Employee
-from inventory_default.models import InventoryDefault
-from inventory_default.models import InventoryDefault
 
-from .forms import InventoryForm
 from .models import Inventory
 from django.contrib.auth.decorators import login_required
 from propensi.utils import is_restoran
-
-@login_required(login_url='/login/')
-def create_inventory(request):
-    if request.method == "POST":
-        add = InventoryForm(request.POST or None)
-        if add.is_valid():
-            emp = Employee.objects.get(user=request.user)
-            idgedung = emp.id_gedung
-            obj = add.save(commit=False)
-            obj.id_gedung = idgedung
-            
-            nama = add.instance.id_inventory_default
-            
-            try:
-                obj.save()
-                messages.success(request, f'Inventori {nama} berhasil dibuat.')
-                return redirect('inventory:create_inventory')
-            except IntegrityError as e:
-                if 'UNIQUE constraint' in str(e.args):
-                    messages.error(request, f'Input gagal. Inventori {nama} sudah terdaftar.')
-                    return redirect('inventory:create_inventory')
-        else:
-            print("Anj")
-                
-    else:
-        add = InventoryForm()    
-    
-    inventory_default = InventoryDefault.objects.all()
-
-    context = {
-        'form': add,
-        'inventory_default': inventory_default,
-    }
-    return render(request, 'create_inventory.html', context)
 
 def update_stok(request,id):
     inventori = Inventory.objects.get(id_inventory=id)
