24b740c85fb2710060e1283e66005da74b8acfcb
retadha <retadha.rumi@gmail.com>
2023-04-17 14:04:41 +0700

    Refactoring dan nambahin comments

--- request/urls.py
+++ request/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import list, confirm, receive, delete
+from .views import list, confirm, receive, delete, accept
 
 app_name = 'request'
 
 urlpatterns = [
     path("", list, name="list"),
+    path("accept", accept, name='accept'),
     path("confirm/<id_request>", confirm, name='confirm'),
     path("receive/<id_request>", receive, name='receive'),
     path("delete/<id_request>", delete, name='delete'),
--- request/views.py
+++ request/views.py
@@ -2,8 +2,10 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect, Http404
 from django.shortcuts import render
 from request.models import Request,Inventory_Line
+from inventory.models import Inventory
 from employee.models import Employee
 from propensi.utils import is_restoran
+from gedung.models import Gedung
 import datetime
 import pywhatkit
 import keyboard
@@ -14,8 +16,18 @@ import keyboard
 def list(request):
     employee = Employee.objects.get(user=request.user)
     gedung = employee.id_gedung
-    requests = Request.objects.filter(id_gedung=gedung)
-    lines = Inventory_Line.objects.all()
+    requests = gedung.request_set.all()
+
+    context = {
+        "requests" : requests,
+        "is_restoran" : is_restoran(request),
+    }
+
+    return render(request, 'request/list.html', context)
+
+@login_required(login_url='/login/')
+def accept(request):
+    requests = Request.objects.filter()
 
     context = {
         "requests" : requests,
@@ -44,13 +56,22 @@ def confirm(request, id_request):
 def receive(request, id_request):
     try:
         inv_request = Request.objects.get(pk=id_request)
-        inv_request.received = datetime.datetime.now()
-        inv_request.status = "3"
     except Request.DoesNotExist:
         raise Http404("Objek tidak ditemukan")
 
-
+    inv_request.received = datetime.datetime.now()
+    inv_request.status = "3"
     inv_request.save()
+
+    gedung = inv_request.id_gedung
+    list_inventori_gedung = gedung.inventory_set.all()
+
+    for line in inv_request.get_lines:
+        inv_gedung = list_inventori_gedung.get(default=line.id_inventory_default)
+        inv_gedung.stok += line.qty
+        inv_gedung.save()
+
+
     return HttpResponseRedirect('/request/')
 
 @login_required(login_url='/login/')
