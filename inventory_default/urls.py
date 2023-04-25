f0bfc1b67f7598aad1ebbe7e4592bb8627d78bed
Ignatius Henriyanto Primai Renda <ignatius.henriyanto@ui.ac.id>
2023-04-25 17:35:18 +0700

    List inventory default

--- inventory_default/urls.py
+++ inventory_default/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import create_inventory_default, delete
+from .views import list_inventory_default, create_inventory_default, delete
 
 app_name = 'inventory_default'
 
 urlpatterns = [
-    path("create/", create_inventory_default, name= "create_inventory_default"),
-    path('delete/<id_inventory_default>', delete, name='delete'),
+    path('', list_inventory_default, name="list_inventory_default"),
+    path("create/", create_inventory_default, name="create_inventory_default"),
+    path('delete/<id_inventory_default>', delete, name='delete_inventory_default'),
 ]
