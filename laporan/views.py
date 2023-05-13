bd5dcb3c27c2239c0abfc976a958c683ea5b2d55
MAULUDI AFINA MIRZA <mauludi.afina@ui.ac.id>
2023-05-13 19:40:28 +0700

    add quantity in hand for fitur laporan

--- laporan/views.py
+++ laporan/views.py
@@ -1,11 +1,28 @@
+from django.http import Http404, HttpResponseRedirect
 from django.shortcuts import redirect, render
+from request.models import Request, Inventory_Line
+from inventory.models import Inventory
+from propensi.utils import is_manager
 from django.contrib.auth.decorators import login_required
+from django.db.models import Sum
 
-@login_required
+
+@login_required(login_url='/login/')
 def pembelian(request):
+    statGedung = request.user.employee.id_gedung.status
+    gedung = request.user.employee.id_gedung
+    status = "PROCESSING"
+    if (not is_manager(request) or statGedung !='0'):
+        return render(request, 'error/403.html')
+    inventory = Inventory.objects.all().filter(id_gedung__exact=gedung)
+    # request = Request.objects.all().filter(status__exact=status).values_list('status')
+    
+    totalStok = inventory.aggregate(Sum('stok')).get('stok__sum')
+    # stokTBR = inventory
     context = {
-        'title': "Transaksi Pembelian",
-        # 'profile': profile
+        'title': "Laporan Pembelian",
+        'totalStok': totalStok,
+        # 'request':request,
     }
     template = 'laporan/pembelian.html'  
     return render(request, template, context)
