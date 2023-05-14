076ed58b45b50a7c100fc34264c3f4362fe627d1
MAULUDI AFINA MIRZA <mauludi.afina@ui.ac.id>
2023-05-14 20:22:41 +0700

    backend laporan pembelian

--- laporan/views.py
+++ laporan/views.py
@@ -7,23 +7,63 @@ from django.contrib.auth.decorators import login_required
 from django.db.models import Sum
 
 
+
 @login_required(login_url='/login/')
 def pembelian(request):
     statGedung = request.user.employee.id_gedung.status
     gedung = request.user.employee.id_gedung
-    status = "PROCESSING"
+    status = '2'
     if (not is_manager(request) or statGedung !='0'):
         return render(request, 'error/403.html')
     inventory = Inventory.objects.all().filter(id_gedung__exact=gedung)
-    # request = Request.objects.all().filter(status__exact=status).values_list('status')
+    
+    wait = waited_orders(self=request)
+    submit = submitted_orders(self=request)
+    process = processed_orders(self=request)
+    complete = completed_orders(self=request)
+    quantityTBR = quantity_TBR(request)
     
     totalStok = inventory.aggregate(Sum('stok')).get('stok__sum')
-    # stokTBR = inventory
     context = {
         'title': "Laporan Pembelian",
         'totalStok': totalStok,
-        # 'request':request,
+        'wait' : wait,
+        'submit' : submit,
+        'process': process,
+        'complete' : complete,
+        'quantityTBR' : quantityTBR,
     }
     template = 'laporan/pembelian.html'  
     return render(request, template, context)
 
+def waited_orders(self, queryset=None):
+    obj = Request.objects.filter(status__exact="0")
+    count = len(obj)
+    return count
+
+def submitted_orders(self, queryset=None):
+    obj = Request.objects.filter(status__exact="1")
+    count = len(obj)
+    return count
+
+def processed_orders(self, queryset=None):
+    obj = Request.objects.filter(status__exact="2")
+    count = len(obj)
+    return count
+
+def completed_orders(self, queryset=None):
+    obj = Request.objects.filter(status__exact="3")
+    count = len(obj)
+    return count
+
+def quantity_TBR(self, queryset=None):
+    obj = Request.objects.filter(status__exact="1").values_list('id_request')
+    jumlah = 0
+    for i in obj:
+        stok = Inventory_Line.objects.filter(id_request__exact=i).aggregate(Sum('qty'))['qty__sum']
+        jumlah += stok
+    
+    if jumlah == None:
+        jumlah = 0
+            
+    return jumlah
