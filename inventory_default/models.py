7eb024b889c135ffe901ef1035937de74eb7c1a2
MAULUDI AFINA MIRZA <mauludi.afina@ui.ac.id>
2023-05-15 15:01:53 +0700

    add column chart (frontend) for laporan pembelian

--- inventory_default/models.py
+++ inventory_default/models.py
@@ -6,7 +6,7 @@ class InventoryDefault(models.Model):
     id_inventory_default = models.AutoField(primary_key=True)
     nama = models.CharField(max_length=50, null=False, blank=False, unique=True, error_messages={'unique':"This name has already been registered."})
     satuan = models.CharField(max_length=20, null=False, blank=False)
-    harga = models.DecimalField(max_digits=20, decimal_places=3, null=False, blank=False)
+    harga = models.DecimalField(max_digits=20, decimal_places=0, null=False, blank=False)
     
     def __str__(self) -> str:
         return self.nama
