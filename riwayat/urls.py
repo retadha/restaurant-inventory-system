e9359c323b22aa5f5293cec230d35ddbcd3420b4
retadha <retadha.rumi@gmail.com>
2023-05-21 17:15:15 +0700

    riwayat pembelian per inventory

--- riwayat/urls.py
+++ riwayat/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from riwayat.views import pembelian
+from riwayat.views import pembelian, pembelian_lines
 
 app_name = 'riwayat'
 
 urlpatterns = [
     path("pembelian", pembelian, name="pembelian"),
+    path("pembelian-lines", pembelian_lines, name="pembelian_lines"),
 
 
 ]
--- riwayat/views.py
+++ riwayat/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
-from request.models import Request
+from request.models import Request, Inventory_Line
 
 @login_required(login_url='/login/')
 def pembelian(request):
@@ -11,3 +11,13 @@ def pembelian(request):
     }
 
     return render(request, 'riwayat/pembelian.html', context)
+
+@login_required(login_url='/login/')
+def pembelian_lines(request):
+    lines = Inventory_Line.objects.filter(id_request__id_gedung__status__exact="0", id_request__status__exact="3")
+    context = {
+        'lines': lines
+
+    }
+
+    return render(request, 'riwayat/lines.html', context)
